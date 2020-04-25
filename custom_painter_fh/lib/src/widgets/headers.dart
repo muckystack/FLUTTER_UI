520058f39b9e3fd4dcf1e2bf45aa106fcbba52c8
muckystack <muckystack@gmail.com>
2020-04-25 13:38:34 -0500

    FEAT: Agrego bordes redondeados

--- custom_painter_fh/lib/src/widgets/headers.dart
+++ custom_painter_fh/lib/src/widgets/headers.dart
@@ -11,3 +11,21 @@ class HeaderCuadrado extends StatelessWidget {
     );
   }
 }
+
+class HeaderBordesRedondeados extends StatelessWidget {
+  const HeaderBordesRedondeados({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      decoration: BoxDecoration(
+        color: Color(0xff615AAB),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(70.0),
+          bottomRight: Radius.circular(70.0)
+          )
+      ),
+    );
+  }
+}
