a8cc4151d52a9da0801317545e26ab8399f1e627
muckystack <muckystack@gmail.com>
2020-05-17 16:26:16 -0500

    FEAT: Implemento el custom painter

--- custom_painter_fh/lib/src/widgets/headers.dart
+++ custom_painter_fh/lib/src/widgets/headers.dart
@@ -29,3 +29,55 @@ class HeaderBordesRedondeados extends StatelessWidget {
     );
   }
 }
+
+class HeaderDiagonal extends StatelessWidget {
+  const HeaderDiagonal({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      // color: Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _HeaderDiagonalPainter(),
+      ),
+    );
+  }
+}
+
+
+class _HeaderDiagonalPainter extends CustomPainter {
+
+  
+  @override
+  void paint(Canvas canvas, Size size) {
+
+    final lapiz = new Paint();
+
+    // Propiedades
+    lapiz.color = Color(0xff615AAB);
+    lapiz.style = PaintingStyle.fill; // .fill Es cuando se rellena
+    lapiz.strokeWidth = 2;
+    
+    final path = new Path();
+
+    // Dibujar con el path y el lapiz
+    path.moveTo(0, size.height * 0.35);
+    path.lineTo(size.width, size.height * 0.30);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+    // path.lineTo(0, size.height * 0.5);
+
+    canvas.drawPath(path, lapiz);
+
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+
+
+
+}
