a9ceabb484cb13b3bbde2486fb63fce10a0d1975
muckystack <muckystack@gmail.com>
2020-05-17 16:56:54 -0500

    FEAT: Header curvo

--- custom_painter_fh/lib/src/widgets/headers.dart
+++ custom_painter_fh/lib/src/widgets/headers.dart
@@ -179,3 +179,54 @@ class _HeaderPuntaPainter extends CustomPainter {
     return true;
   }
 }
+
+
+class HeaderCurvo extends StatelessWidget {
+  const HeaderCurvo({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      // color: Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _HeaderCurvoPainter(),
+      ),
+    );
+  }
+}
+
+
+class _HeaderCurvoPainter extends CustomPainter {
+
+  
+  @override
+  void paint(Canvas canvas, Size size) {
+
+    final lapiz = new Paint();
+
+    // Propiedades
+    lapiz.color = Color(0xff615AAB);
+    lapiz.style = PaintingStyle.fill; // .fill Es cuando se rellena
+    lapiz.strokeWidth = 20;
+    
+    final path = new Path();
+
+    // Dibujar con el path y el lapiz
+    path.lineTo(0, size.height * 0.25);
+    // path.quadraticBezierTo(size.width * 0.5, size.height * .40, size.width, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.5, size.height * .20, size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+
+
+
+    canvas.drawPath(path, lapiz);
+
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
