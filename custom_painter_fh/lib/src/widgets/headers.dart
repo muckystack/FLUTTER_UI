2829cbadef45b1f1419625e04ff41e731aa62a98
muckystack <muckystack@gmail.com>
2020-05-17 17:05:23 -0500

    FEAT. custom painter ola

--- custom_painter_fh/lib/src/widgets/headers.dart
+++ custom_painter_fh/lib/src/widgets/headers.dart
@@ -230,3 +230,54 @@ class _HeaderCurvoPainter extends CustomPainter {
     return true;
   }
 }
+
+
+class HeaderWave extends StatelessWidget {
+  const HeaderWave({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      // color: Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _HeaderWavePainter(),
+      ),
+    );
+  }
+}
+
+
+class _HeaderWavePainter extends CustomPainter {
+
+  
+  @override
+  void paint(Canvas canvas, Size size) {
+
+    final lapiz = new Paint();
+
+    // Propiedades
+    lapiz.color = Color(0xff615AAB);
+    lapiz.style = PaintingStyle.fill; // .fill Es cuando se rellena
+    lapiz.strokeWidth = 20;
+    
+    final path = new Path();
+
+    // Dibujar con el path y el lapiz
+    path.lineTo(0, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.25, size.height * .30, size.width * .5, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.75, size.height * .20, size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+
+
+
+    canvas.drawPath(path, lapiz);
+
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
