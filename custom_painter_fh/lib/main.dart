d7e72ae40fa54929940373ca4a8f7ba12859ae91
muckystack <muckystack@gmail.com>
2020-11-13 23:43:52 -0600

    38. Inicio de un nuevo reto

--- custom_painter_fh/lib/main.dart
+++ custom_painter_fh/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:custompainterfh/src/pages/animaciones_page.dart';
 import 'package:custompainterfh/src/pages/headers_page.dart';
+import 'package:custompainterfh/src/retos/cuadrado_animado_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,6 +8,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Diseños App', home: AnimacionesPage());
+    return MaterialApp(title: 'Diseños App', home: CuadradoAnimadoPage());
   }
 }
---
+++ custom_painter_fh/lib/src/retos/cuadrado_animado_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class CuadradoAnimadoPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: _CuadradoAnimado()),
+    );
+  }
+}
+
+class _CuadradoAnimado extends StatefulWidget {
+  const _CuadradoAnimado({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  __CuadradoAnimadoState createState() => __CuadradoAnimadoState();
+}
+
+class __CuadradoAnimadoState extends State<_CuadradoAnimado> {
+  AnimationController controller;
+  // animaciones
+
+  @override
+  void initState() {
+    // Inicializar todo
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _Rectangulo();
+  }
+}
+
+class _Rectangulo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 70,
+      height: 70,
+      decoration: BoxDecoration(color: Colors.blue),
+    );
+  }
+}
