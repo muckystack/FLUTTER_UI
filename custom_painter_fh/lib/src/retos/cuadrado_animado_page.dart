04801c580d86dd10f241f6d66ad613f34940513f
muckystack <muckystack@gmail.com>
2020-11-13 23:57:29 -0600

    39. Reto y solución

--- custom_painter_fh/lib/src/retos/cuadrado_animado_page.dart
+++ custom_painter_fh/lib/src/retos/cuadrado_animado_page.dart
@@ -18,13 +18,43 @@ class _CuadradoAnimado extends StatefulWidget {
   __CuadradoAnimadoState createState() => __CuadradoAnimadoState();
 }
 
-class __CuadradoAnimadoState extends State<_CuadradoAnimado> {
+class __CuadradoAnimadoState extends State<_CuadradoAnimado>
+    with SingleTickerProviderStateMixin {
   AnimationController controller;
   // animaciones
+  Animation<double> moverDerecha;
+  Animation<double> moverIzquierda;
+  Animation<double> moverArriba;
+  Animation<double> moverAbajo;
 
   @override
   void initState() {
     // Inicializar todo
+    controller = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 4500));
+
+    moverDerecha = Tween(begin: 0.0, end: 100.0).animate(CurvedAnimation(
+        parent: controller,
+        curve: Interval(0.0, 0.25, curve: Curves.bounceOut)));
+
+    moverArriba = Tween(begin: 0.0, end: -100.0).animate(CurvedAnimation(
+        parent: controller,
+        curve: Interval(0.25, 0.5, curve: Curves.bounceOut)));
+
+    moverIzquierda = Tween(begin: 0.0, end: -100.0).animate(CurvedAnimation(
+        parent: controller,
+        curve: Interval(0.5, 0.75, curve: Curves.bounceOut)));
+
+    moverAbajo = Tween(begin: 0.0, end: 100.0).animate(CurvedAnimation(
+        parent: controller,
+        curve: Interval(0.75, 1.0, curve: Curves.bounceOut)));
+
+    controller.addListener(() {
+      if (controller.status == AnimationStatus.completed) {
+        controller.reset();
+      }
+    });
+
     super.initState();
   }
 
@@ -36,7 +66,19 @@ class __CuadradoAnimadoState extends State<_CuadradoAnimado> {
 
   @override
   Widget build(BuildContext context) {
-    return _Rectangulo();
+    controller.forward();
+
+    return AnimatedBuilder(
+      animation: controller,
+      child: _Rectangulo(),
+      builder: (BuildContext context, Widget child) {
+        return Transform.translate(
+          offset: Offset(moverDerecha.value + moverIzquierda.value,
+              moverArriba.value + moverAbajo.value),
+          child: child,
+        );
+      },
+    );
   }
 }
 
