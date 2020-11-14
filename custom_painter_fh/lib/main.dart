47b5db8a9385ddc800e87e3e545547c697120105
muckystack <muckystack@gmail.com>
2020-11-13 22:09:15 -0600

    FEAT: Estructura inicial

--- custom_painter_fh/lib/main.dart
+++ custom_painter_fh/lib/main.dart
@@ -1,14 +1,12 @@
+import 'package:custompainterfh/src/pages/animaciones_page.dart';
 import 'package:custompainterfh/src/pages/headers_page.dart';
 import 'package:flutter/material.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Diseños App',
-      home: HeadersPage()
-    );
+    return MaterialApp(title: 'Diseños App', home: AnimacionesPage());
   }
 }
